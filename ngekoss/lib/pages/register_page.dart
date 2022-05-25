2e2d54547bf65f09f59d40f216a9466ef82334de
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-25 18:51:12 +0800

    register fix

--- ngekoss/lib/pages/register_page.dart
+++ ngekoss/lib/pages/register_page.dart
@@ -88,15 +88,189 @@ class RegisterPage extends StatelessWidget {
       );
     }
 
+    inputEmail() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: subText,
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Color(0xffF1F0F5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18, top: 10),
+                child: TextFormField(
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Masukkan Email',
+                    hintStyle: GoogleFonts.poppins(
+                      color: subText,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    inputPassword() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: subText,
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Color(0xffF1F0F5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18, top: 10),
+                child: TextFormField(
+                  obscureText: true,
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Masukkan Password',
+                    hintStyle: GoogleFonts.poppins(
+                      color: subText,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    inputNoHP() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'No HP',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: subText,
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Color(0xffF1F0F5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18, top: 10),
+                child: TextFormField(
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Masukkan Nomor HP',
+                    hintStyle: GoogleFonts.poppins(
+                      color: subText,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    btnDaftar() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 24),
+        height: 45,
+        width: double.infinity,
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: warnaOren,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(66),
+            ),
+          ),
+          onPressed: () {},
+          child: Text(
+            'Daftar',
+            style: GoogleFonts.poppins(
+              color: warnaPutih,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    btnLogin() {
+      return Container(
+        child: TextButton(
+          onPressed: () {},
+          child: Text(
+            'Login Sekarang',
+            style: GoogleFonts.poppins(
+              fontWeight: light,
+              color: subText,
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       body: SafeArea(
         child: ListView(
+          padding: EdgeInsets.only(bottom: 40),
           children: [
             title(),
             SizedBox(height: 40),
             gambardaftar(),
             SizedBox(height: 40),
-            inputNamaLengkap()
+            inputNamaLengkap(),
+            SizedBox(height: 20),
+            inputEmail(),
+            SizedBox(height: 20),
+            inputPassword(),
+            SizedBox(height: 20),
+            inputNoHP(),
+            SizedBox(height: 40),
+            btnDaftar(),
+            SizedBox(height: 20),
+            btnLogin(),
           ],
         ),
       ),
